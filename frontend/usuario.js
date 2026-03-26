99bae5b4c5ac1f53134169165036197b350aa1a2
MATHEUS DONAVAN VAHLDICK CUNHA <matheus_d_cunha@estudante.sesisenai.org.br>
2026-03-26 17:14:42 -0300

    feat: finalizado comunicação

--- frontend/usuario.js
+++ frontend/usuario.js
@@ -1,12 +1,14 @@
-window.onload = ()=>{
+window.onload = () => {
     buscarUsuarios()
 }
 
+let _usuarios = []
+let _usuario_editado = null;
 
 // ============================
 // Criar Conta
 // ============================
-function createAccount(event) {
+function salvar(event) {
     event.preventDefault();
 
     const nome = document.getElementById("nome").value;
@@ -15,39 +17,60 @@ function createAccount(event) {
     const email = document.getElementById("email").value;
     const usuario = document.getElementById("usuario").value.toLowerCase();
     const senha = document.getElementById("senha").value;
-    fetch("http://localhost:1880/usuario/criar",{
-        method:"POST",
-        body:JSON.stringify({nome,sobrenome,nascimento,email,usuario,senha})
-    }).then((resposta)=>{
-        console.log(resposta)
-        if(resposta.ok){
-            resposta.json()
-        }
-    }).then((usuario)=>{
-        alert("Enviado e recebido")
-        window.location.href = "usuario.html";
+
+    if (_usuario_editado) {
+        //Está alterando
+        fetch("http://localhost:1880/alterar/usuario", {
+        method: "PATCH",
+        body: JSON.stringify({ nome, sobrenome, nascimento, email, usuario, senha,id:_usuario_editado })
+    }).then(()=>{
+        _usuario_editado = null
     })
+
+
+
+    } else {
+        //Está criando
+
+        fetch("http://localhost:1880/usuario/criar", {
+            method: "POST",
+            body: JSON.stringify({ nome, sobrenome, nascimento, email, usuario, senha })
+        }).then((resposta) => {
+            console.log(resposta)
+            if (resposta.ok) {
+                resposta.json()
+            }
+        }).then((usuario) => {
+            alert("Enviado e recebido")
+            window.location.href = "usuario.html";
+        })
+
+
+
+    }
+
 }
 
-async function buscarUsuarios(){
-    try{
+async function buscarUsuarios() {
+    try {
         let resposta = await fetch("http://localhost:1880/buscar/usuario");
-        if(!resposta.ok){
+        if (!resposta.ok) {
             throw new Error("Erro ao buscar")
         }
         let dados = await resposta.json()
         console.log(dados)
+        _usuarios = dados;
         listaUsuarios(dados)
-    }catch(e){
+    } catch (e) {
         console.error(e)
     }
 }
 
-function listaUsuarios(usuarios){
-    
+function listaUsuarios(usuarios) {
+
     let tabela = document.getElementById("listaUsuarios")
 
-    usuarios.forEach((usuario)=>{
+    usuarios.forEach((usuario) => {
 
         let data = new Date(usuario.dt_nascimento);
 
@@ -59,7 +82,7 @@ function listaUsuarios(usuarios){
             <td>${usuario.usuario}</td>
             <td>${usuario.email}</td>
             <td><button onclick="carregarDados(${usuario.id})">Alterar</button></td>
-            <td>EXCLUIR</td>
+            <td><button onclick="excluirDados(${usuario.id})">Excluir</td>
         </tr>
         `
 
@@ -68,22 +91,49 @@ function listaUsuarios(usuarios){
 
 }
 
-function carregarDados(id){
+function carregarDados(id) {
 
+    _usuario_editado = id;
+
+    _usuarios.forEach(usuario=>{
+        if(usuario.id == id){
+            document.getElementById('nome').value = usuario.nome;
+            document.getElementById("sobrenome").value = usuario.sobrenome
+            let data = new Date(usuario.dt_nascimento)
+            document.getElementById("nascimento").value = data
+            document.getElementById("email").value = usuario.email
+            document.getElementById("usuario").value = usuario.usuario
+        }
+    })
+
+    
 }
 
+function excluirDados(id) {
+
+
+    if (confirm("Deseja realmente excluir essa conta?")) {
+        console.log("Conta excluída");
+        fetch("http://localhost:1880/excluir/usuario", {
+            method: "DELETE",
+            body: JSON.stringify({ id })
+        })
+    } else {
+        console.log("Ação cancelada");
+    }
+}
 
 // ============================
 // Login
 // ============================
 
-async function logar(e){
+async function logar(e) {
     e.preventDefault();
 
     let input_email = document.getElementById('email');
     let input_senha = document.getElementById('senha');
 
-    if(!input_email || !input_senha){
+    if (!input_email || !input_senha) {
         alert("Inputs não encontrados")
         return;
     }
@@ -91,21 +141,21 @@ async function logar(e){
     let email = input_email.value;
     let senha = input_senha.value;
 
-    try{
+    try {
 
-        let resposta = await fetch("http://localhost:1880/Autenticar",{
-            method:'POST',
-            body:JSON.stringify({email,senha})
+        let resposta = await fetch("http://localhost:1880/Autenticar", {
+            method: 'POST',
+            body: JSON.stringify({ email, senha })
             //body:{email,senha}
         })
 
-        if(resposta.status == 200){
+        if (resposta.status == 200) {
             alert("Entrou");
             window.location.href = "./bancadas/bancadas.html";
-        }else{
+        } else {
             alert("Usuário ou senha inválidos")
         }
-    }catch(erro){
+    } catch (erro) {
         alert("Erro ao buscar, confira o console para ver mais detalhes.")
         console.error(erro);
     }
